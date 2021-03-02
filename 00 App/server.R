102f6fc8268386801fd4de8e41f0c3c3cc668176
aidanmorrison <aidan.morrison@phs.scot>
2021-03-02 16:24:03 +0000

    Building dynamic input form

--- 00 App/server.R
+++ 00 App/server.R
@@ -1,15 +1,31 @@
 
-# Define server logic required to draw a histogram
-server <- function(input, output) {
+server <- function(input, output, session) {
+  
+  ################################################
+  ## Data Explorer 
+  
+  ## input form
+  source("scripts/outputs/ui_main_inputs.R", local = TRUE)$value
+
+  ################################################
   
   data <- reactive({
-    resource_id <- filter(resource_lookup, resource_description == input$resource) %>%
+    
+    if(length(input$resource_desc == 1)){
+
+    selected_resource <- input$resource_desc
+
+    resource_id <- filter(resource_lookup, resource_description == selected_resource) %>%
       pull(resource_id)
-    get_data(resource_id = resource_id) 
-  })
-  
+    data <- get_data(resource_id = resource_id)
+    data$result$records
+    
+    }
+    
+    })
+
   output$output_table <- renderDataTable({
     data()
   })
-    
+
 }
